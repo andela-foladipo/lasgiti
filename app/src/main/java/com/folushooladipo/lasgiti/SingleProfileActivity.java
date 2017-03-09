4398988863dcfd71c56489343be6853fb4496de4
Folusho <folusho@lanthanidesoft.com>
2017-03-09 18:51:16 +0100

    Added color to a ProgressBar; removed unused imports.

--- app/src/main/java/com/folushooladipo/lasgiti/SingleProfileActivity.java
+++ app/src/main/java/com/folushooladipo/lasgiti/SingleProfileActivity.java
@@ -1,18 +1,19 @@
 package com.folushooladipo.lasgiti;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
@@ -51,6 +52,9 @@ public class SingleProfileActivity extends AppCompatActivity {
         profile = getIntent().getBundleExtra("profile");
         userId = profile.getString("userId");
         this.setTitle("Profile of: " + userId);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.single_profile_loading_detailed_profile_progress_bar);
+        progressBar.getIndeterminateDrawable().setColorFilter(Color.parseColor("#e93ad4"), PorterDuff.Mode.SRC_IN);
 
         profilePicture = (SimpleDraweeView) findViewById(R.id.single_profile_picture);
         profilePicture.setImageURI(profile.getString("profilePicture"));
