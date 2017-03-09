42c9a8ff74b74d319dfa92d5c53e16e7085d4d64
Folusho <folusho@lanthanidesoft.com>
2017-03-09 18:45:01 +0100

    Implemented infinite scrolling.

--- app/src/main/java/com/folushooladipo/lasgiti/ProfilesActivity.java
+++ app/src/main/java/com/folushooladipo/lasgiti/ProfilesActivity.java
@@ -1,38 +1,49 @@
 package com.folushooladipo.lasgiti;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class ProfilesActivity extends AppCompatActivity {
     private int pageNumber;
+    private int PROFILES_PER_PAGE = 50;
+    private int TOTAL_AVAILABLE_PROFILES = -1;
     private final String GITHUB_SEARCH_API = "https://api.github.com/search/users";
     private final String GITHUB_SEARCH_API_DEFAULT_QUERY_STRING = "q=location:lagos+type:user+language:java&order=desc&sort=repositories&per_page=50&";
     private final String GITHUB_SEARCH_API_DEFAULT_URL = GITHUB_SEARCH_API +
             "?access_token=" + SensitiveData.GITHUB_ACCESS_TOKEN + "&" + GITHUB_SEARCH_API_DEFAULT_QUERY_STRING;
 
+    private Boolean isLoadingProfiles;
     private LinearLayout loadingContainer;
+    private TextView loadingView;
     private LinearLayout loadingFailedContainer;
     private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
+    private LinearLayoutManager layoutManager;
     private RecyclerView.Adapter adapter;
     private ArrayList<JSONObject> profilesDataset = new ArrayList<JSONObject>();
     private FetchGitHubProfilesTask fetchProfilesTask;
@@ -42,8 +53,16 @@ public class ProfilesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
 
-        pageNumber = 0;
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.profiles_activity_loading_progress_bar);
+        progressBar.getIndeterminateDrawable().setColorFilter(Color.parseColor("#e93ad4"), PorterDuff.Mode.SRC_IN);
+
+        // IMPORTANT: This is only incremented after a successful retrieval of profiles.
+        pageNumber = 1;
+        // isLoadingProfiles is especially useful in the infinite scrolling functionality because
+        // the RecyclerView's onScrollListener gets called many times for EACH scroll by the user.
+        isLoadingProfiles = false;
         loadingContainer = (LinearLayout) findViewById(R.id.profiles_activity_loading_container);
+        loadingView = (TextView) findViewById(R.id.profiles_activity_loading);
         loadingFailedContainer = (LinearLayout) findViewById(R.id.profiles_activity_loading_failed_container);
         recyclerView = (RecyclerView) findViewById(R.id.profiles_activity_profiles_container);
         layoutManager = new LinearLayoutManager(this);
@@ -63,6 +82,44 @@ public class ProfilesActivity extends AppCompatActivity {
                 loadingContainer.setVisibility(View.VISIBLE);
                 loadProfiles(pageNumber);
 
+            }
+        });
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                // Only when the user scrolls down.
+                if (dy > 0) {
+                    int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
+                    int numberOfItems = adapter.getItemCount();
+                    // Remember that lastVisibleItem is a zero-based index.
+                    if (lastVisibleItem == (numberOfItems - 1)) {
+                        if (pageNumber == 0) {
+                            loadingView.setText(R.string.loading_more_profiles_msg);
+                        }
+                        loadingContainer.setVisibility(View.VISIBLE);
+                        int numberOfProfilesFetched = pageNumber * PROFILES_PER_PAGE;
+                        Log.i("profileCount", "" + numberOfProfilesFetched);
+                        if (!isLoadingProfiles) {
+                            if (numberOfProfilesFetched < TOTAL_AVAILABLE_PROFILES
+                                    && numberOfProfilesFetched < Utilities.GITHUB_SEARCH_RESULTS_LIMIT) {
+                                isLoadingProfiles = true;
+                                loadProfiles(pageNumber);
+                            }
+                            else {
+                                loadingContainer.setVisibility(View.GONE);
+                                if (numberOfProfilesFetched >= TOTAL_AVAILABLE_PROFILES) {
+                                    Toast.makeText(getApplicationContext(), "No more profiles to display.", Toast.LENGTH_SHORT).show();
+                                }
+                                if (numberOfProfilesFetched >= Utilities.GITHUB_SEARCH_RESULTS_LIMIT) {
+                                    Toast.makeText(getApplicationContext(), "You can only view 1000 profiles.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    }
+                }
             }
         });
 
@@ -148,12 +205,7 @@ public class ProfilesActivity extends AppCompatActivity {
             try {
                 result = Utilities.downloadUrl(urlToQuery);
             }
-            catch (IOException e) {
-                e.printStackTrace();
-                result = "ERROR: " + e.getMessage();
-            }
             catch (Exception e) {
-                e.printStackTrace();
                 result = "ERROR: " + e.getMessage();
             }
             return result;
@@ -161,31 +213,37 @@ public class ProfilesActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-
+            Log.i("url", urlToQuery);
+            loadingContainer.setVisibility(View.GONE);
             if (result.substring(0, 5).contains("ERROR")) {
                 // Show an error message and a "RETRY" button.
                 loadingFailedContainer.setVisibility(View.VISIBLE);
-                loadingContainer.setVisibility(View.GONE);
             }
             else {
                 try {
                     JSONObject resultObj = new JSONObject(result);
+                    // Assign once.
+                    if (TOTAL_AVAILABLE_PROFILES == -1) {
+                        TOTAL_AVAILABLE_PROFILES = resultObj.getInt("total_count");
+                    }
                     JSONArray profiles = resultObj.getJSONArray("items");
                     for (int i = 0; i < profiles.length(); i++) {
                         JSONObject profile = (JSONObject) profiles.get(i);
                         profilesDataset.add(profile);
                     }
                     adapter.notifyDataSetChanged();
+                    isLoadingProfiles = false;
                     pageNumber++;
                 }
                 catch (JSONException e) {
-                    e.printStackTrace();
+                    loadingFailedContainer.setVisibility(View.VISIBLE);
                 }
             }
         }
     }
 
     private void loadProfiles(int page) {
+        Log.i("pagination", "" + page);
         String url = GITHUB_SEARCH_API_DEFAULT_URL + "page=" + page + "&";
         fetchProfilesTask = new FetchGitHubProfilesTask(url);
         fetchProfilesTask.execute();
